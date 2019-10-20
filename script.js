721faecbc343de723f2f68b1ca3e13193cd78151
Andrei Chapiuk <venik2281488@gmail.com>
2019-10-20 20:16:33 +0300

    feat: final version task, add swiper

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
-function down()
-{
+function down() {
   var a = document.getElementById('dropdown');
-  if ( a.style.display == 'none' )
+  if (a.style.display == 'none')
     a.style.display = 'block'
   else
-    if ( a.style.display == 'block' )
-    a.style.display = 'none';
+    if (a.style.display == 'block')
+      a.style.display = 'none';
+}
+
+window.onload = function () {
+  //initialize swiper when document ready
+  var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true
+  })
 };
+function list() {
+  var a = document.getElementById('inf_one');
+  if (a.style.display == 'none')
+    a.style.display = 'block'
+  else
+    if (a.style.display == 'block')
+      a.style.display = 'none';
+};
+
+function listTwo() {
+  var a = document.getElementById('inform_two');
+  if (a.style.display == 'none')
+    a.style.display = 'block'
+  else
+    if (a.style.display == 'block')
+      a.style.display = 'none';
+}
